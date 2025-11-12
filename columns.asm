46f002e1f74c7187eba250a2c0b1f00f1f6d5b0e
Junyu Li <142877115+Jasjas7777@users.noreply.github.com>
2025-11-12 10:11:28 -0500

    Implement movement by keyboard

--- columns.asm
+++ columns.asm
@@ -26,6 +26,8 @@ ADDR_DSPL:
 ADDR_KBRD:
     .word 0xffff0000
 colors: .word 0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xffaa00, 0xaa00ff
+columnColor: .space 12
+
 
 
 ##############################################################################
@@ -39,6 +41,11 @@ colors: .word 0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xffaa00, 0xaa00ff
 	.globl main
     # Run the game.
 main:
+la $s1, columnColor
+add $s2, $zero, 6 #Initial X offset
+add $s3, $zero, 4 #Initial Y offset
+
+
 #Draw top border
     lw $t0 ADDR_DSPL
     addi $t0, $t0, 392
@@ -83,12 +90,15 @@ drawBorderRight:
     addi $t1, $t1, -1
     bgez $t1, drawBorderRight
     
-#Draw Preview column
+#Draw a column
     lw $t0, ADDR_DSPL
-    addi $t0, $t0, 728
+    mul $t4, $s2, 4 #x position
+    mul $t5, $s3, 128 #y posiiton
+    add $t0, $t0, $t4 #Add these offset to initial position
+    add $t0, $t0, $t5
     addi $t1, $zero, 2 # 3 cells
     la $t2 colors
-drawPreview: 
+drawColumn: 
     #Generate a random number
     li $v0, 42		# choose a random number
     li $a0, 0
@@ -97,22 +107,72 @@ drawPreview:
 	
 	#Get the random color
 	mul $t3, $a0, 4
-	add $t5, $t2, $t3 
-	lw $t4, 0($t5) #the random color we got
-	sw $t4, 0($t0)
+	add $t7, $t2, $t3 
+	lw $t6, 0($t7) #the random color we got
+	sw $t6, 0($t0) #Draw the color
+	sw $t6, 0($s1) #Save the color in a column array
 	addi $t1, $t1, -1
 	addi $t0, $t0, 128
-	bgez $t1 drawPreview
+	addi $s1, $s1, 4
+	bgez $t1 drawColumn
 	
-	
-	
-	
-    
-        
+#Draw the column in new position
+moveColumn:
+    la $s1, columnColor 
+    addi $t1, $zero, 2 # 3 cells
+    lw $t0, ADDR_DSPL
+    mul $t4, $s2, 4 #x position
+    mul $t5, $s3, 128 #y posiiton
+    add $t0, $t0, $t4 #Add these offset to initial position
+    add $t0, $t0, $t5
+moveColumnLoop:
+	add $t7, $s1, $t3 
+	lw $t6, 0($s1)
+	sw $t6, 0($t0) #Draw the color
+    addi $t1, $t1, -1
+    addi $t0, $t0, 128
+    addi $s1, $s1, 4
+    bgez $t1 moveColumnLoop
+
     
     
 game_loop:
     # 1a. Check if key has been pressed
+    lw $t1, ADDR_KBRD               # $t0 = base address for keyboard
+    lw $t8, 0($t1)                  # Load first word from keyboard
+    beq $t8, 1, keyboard_input      # If first word 1, key is pressed
+    bne  $t8, 1, game_loop
+keyboard_input:                     # A key is pressed
+    lw $a0, 4($t1)                  # Load second word from keyboard
+    beq $a0, 0x77, w_shuffle     # Check if the key w was pressed
+    beq $a0, 0x61, a_moveLeft     # Check if the key a was pressed
+    beq $a0, 0x73, s_moveDown     # Check if the key s was pressed
+    beq $a0, 0x64, d_moveRight     # Check if the key d was pressed
+    beq $a0, 0x71, respond_to_Q     # Check if the key q was pressed
+
+    li $v0, 1                       # ask system to print $a0
+    syscall
+    b main
+    
+w_shuffle:
+
+a_moveLeft:
+    jal eraseColumn
+    addi $s2, $s2, -1
+    j moveColumn
+s_moveDown:
+    jal eraseColumn
+    addi $s3, $s3, 1
+    j moveColumn
+    
+d_moveRight:  
+    jal eraseColumn
+    addi $s2, $s2, 1
+    j moveColumn
+respond_to_Q:
+	li $v0, 10                      # Quit gracefully
+	syscall
+
     # 1b. Check which key has been pressed
     # 2a. Check for collisions
 	# 2b. Update locations (capsules)
@@ -121,3 +181,19 @@ game_loop:
 
     # 5. Go back to Step 1
     j game_loop
+    
+#Erase the old column
+
+eraseColumn:
+    lw $t0, ADDR_DSPL
+    mul $t4, $s2, 4 #x position
+    mul $t5, $s3, 128 #y posiiton
+    add $t0, $t0, $t4 #Add these offset to initial position
+    add $t0, $t0, $t5
+    addi $t1, $zero, 2 # 3 cells
+eraseLoop:
+    sw $zero, 0($t0) #Draw the color
+    addi $t1, $t1, -1
+	addi $t0, $t0, 128
+	bgez $t1 eraseLoop
+	jr $ra
